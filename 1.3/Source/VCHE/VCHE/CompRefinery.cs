c4979fb6dafe85a7e3ab10d347e6d5b40171d25b
KylianB <kylian.balde@gmail.com>
2022-04-02 14:09:06 +0200

    Deepchem net

---
+++ 1.3/Source/VCHE/VCHE/CompRefinery.cs
@@ -0,0 +1,39 @@
+﻿using PipeSystem;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VCHE
+{
+    public class CompProperties_Refinery : CompProperties
+    {
+        public CompProperties_Refinery()
+        {
+            compClass = typeof(CompRefinery);
+        }
+    }
+
+    public class CompRefinery : ThingComp
+    {
+        private CompResourceProcessor resource;
+        private Vector3 fireDrawPos;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            resource = parent.GetComp<CompResourceProcessor>();
+
+            fireDrawPos = parent.DrawPos;
+            fireDrawPos.y += 3f / 74f;
+            fireDrawPos.z += 1.25f;
+        }
+
+        public override void PostDraw()
+        {
+            if (resource.Working)
+            {
+                CompFireOverlay.FireGraphic.Draw(fireDrawPos, Rot4.North, parent);
+            }
+        }
+    }
+}
