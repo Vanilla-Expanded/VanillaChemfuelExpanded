2f1f97c68fe3fe2e21117be8ffacd0336f625df1
KylianB <kylian.balde@gmail.com>
2022-04-03 10:28:30 +0200

    Pumpjack placeworker

---
+++ 1.3/Source/VCHE/VCHE/PlaceWorker_Pumpjack.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace VCHE
+{
+    public class PlaceWorker_Pumpjack : PlaceWorker_ShowDeepResources
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            bool canPlace = false;
+            var cells = GenRadial.RadialCellsAround(loc, checkingDef.specialDisplayRadius, true);
+            for (int i = 0; i < cells.Count(); i++)
+            {
+                var cell = cells.ElementAt(i);
+                if (map.deepResourceGrid.ThingDefAt(cell) is ThingDef thingDef && thingDef.defName == "VCHE_Deepchem")
+                {
+                    canPlace = true;
+                }
+            }
+            return canPlace;
+        }
+    }
+}
