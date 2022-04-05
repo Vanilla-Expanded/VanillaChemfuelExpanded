07f957ae004c3a530633e225026f0c455a8ab8a1
KylianB <kylian.balde@gmail.com>
2022-04-05 15:20:17 +0200

    Better pumpjack and animation cycle

--- 1.3/Source/VCHE/VCHE/CompPumpjack.cs
+++ 1.3/Source/VCHE/VCHE/CompPumpjack.cs
@@ -24,16 +24,17 @@ namespace VCHE
         private static readonly Material PumpjackPump = MaterialPool.MatFrom("Things/Building/Production/DeepchemPumpjack_Pump");
 
         private Vector3 pumpPos = Vector3.zero;
+        private Vector3 pumpPosMax;
         private Vector3 bottomPos;
         private Vector3 trueCenter;
 
-        private CompResource resource;
+        private CompResourceStorage resource;
         private CompPowerTrader powerComp;
 
         private int nextProduceTick = -1;
-        private bool noStorage = false;
         private bool noCapacity = false;
         private bool goingUp = true;
+        private bool cycleOver = true;
         private List<IntVec3> adjCells;
 
         public CompProperties_Pumpjack Props => (CompProperties_Pumpjack)props;
@@ -41,15 +42,16 @@ namespace VCHE
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             base.PostSpawnSetup(respawningAfterLoad);
-            resource = parent.GetComp<CompResource>();
+            resource = parent.GetComp<CompResourceStorage>();
             powerComp = parent.GetComp<CompPowerTrader>();
             adjCells = adjCells = GenAdj.CellsAdjacent8Way(parent).ToList();
 
             trueCenter = parent.TrueCenter();
             if (pumpPos == Vector3.zero)
-                pumpPos = trueCenter + new Vector3(0, 0.75f, 0);
+                pumpPos = trueCenter + new Vector3(0f, 0.75f, 0f);
 
-            bottomPos = trueCenter + new Vector3(0, 1f, 0);
+            bottomPos = trueCenter + new Vector3(0f, 1f, 0f);
+            pumpPosMax = trueCenter + new Vector3(0f, 0f, 1.1f);
         }
 
         public override void PostDeSpawn(Map map)
@@ -61,8 +63,8 @@ namespace VCHE
         {
             Scribe_Values.Look(ref nextProduceTick, "nextProduceTick", 0);
             Scribe_Values.Look(ref noCapacity, "noCapacity", false);
-            Scribe_Values.Look(ref noStorage, "noStorage", false);
-            Scribe_Values.Look(ref goingUp, "goingUp", true);
+            Scribe_Values.Look(ref cycleOver, "cycleOver");
+            Scribe_Values.Look(ref goingUp, "goingUp");
             Scribe_Values.Look(ref pumpPos, "pumpPos");
         }
 
@@ -72,7 +74,7 @@ namespace VCHE
             if (parent.Spawned)
             {
                 var ticksGame = Find.TickManager.TicksGame;
-                if (ticksGame == -1)
+                if (nextProduceTick == -1)
                 {
                     nextProduceTick = ticksGame + Props.ticksPerPortion;
                 }
@@ -82,27 +84,22 @@ namespace VCHE
                     nextProduceTick = ticksGame + Props.ticksPerPortion;
                 }
 
-                if (powerComp.PowerOn)
+                if (!cycleOver)
                 {
-                    if (pumpPos.z > trueCenter.z + 1.1f || pumpPos.z < trueCenter.z)
-                    {
-                        goingUp = !goingUp;
-                    }
-
                     if (goingUp)
-                        pumpPos.z += 0.01f;
-                    else
-                        pumpPos.z -= 0.03f;
-                }
-                else
-                {
-                    if (pumpPos.z > trueCenter.z)
                     {
-                        pumpPos.z -= 0.03f;
+                        pumpPos.z += 0.01f;
+                        if (pumpPos.z > pumpPosMax.z)
+                            goingUp = false;
                     }
                     else
                     {
-                        goingUp = true;
+                        pumpPos.z -= 0.03f;
+                        if (pumpPos.z < trueCenter.z)
+                        {
+                            cycleOver = true;
+                            goingUp = true;
+                        }
                     }
                 }
             }
@@ -110,86 +107,67 @@ namespace VCHE
 
         private void TryProducePortion()
         {
+            // No power -> Return
             if (powerComp != null && !powerComp.PowerOn)
-            {
                 return;
-            }
+            // Get resource
+            bool nextResource = GetNextResource(out ThingDef resDef, out int count, out IntVec3 cell);
+            // Resource isn't deepchem -> Return
+            if (resDef == null || resDef.defName != "VCHE_Deepchem")
+                return;
 
+            var map = parent.Map;
+            // Resource comp is here
             if (resource != null)
             {
                 var net = resource.PipeNet;
                 if (net.connectors.Count > 1)
                 {
-                    if (net.storages.Count == 0)
+                    noCapacity = net.AvailableCapacity == 0;
+
+                    if (!noCapacity)
                     {
-                        noStorage = true;
-                        return;
-                    }
-                    else
-                    {
-                        noStorage = false;
+                        parent.Map.deepResourceGrid.SetAt(cell, resDef, count - 1);
+                        net.DistributeAmongStorage(1);
+
+                        if (cycleOver) cycleOver = false;
                     }
 
-                    if (net.AvailableCapacity == 0)
-                    {
-                        noCapacity = true;
-                        return;
-                    }
-                    else
-                    {
-                        noCapacity = false;
-                    }
-
-                    bool b = GetNextResource(out ThingDef def, out int count, out IntVec3 c);
-                    if (def == null || def.defName != "VCHE_Deepchem")
-                    {
-                        return;
-                    }
-                    if (b)
-                    {
-                        parent.Map.deepResourceGrid.SetAt(c, def, count - 1);
-                    }
-                    net.DistributeAmongStorage(1);
                     return;
                 }
             }
-
-            bool nextResource = GetNextResource(out ThingDef resDef, out int countPresent, out IntVec3 cell);
-            if (resDef == null || resDef.defName != "VCHE_Deepchem")
-            {
-                return;
-            }
-
+            // If there is no resource comp
+            // Deplete resource by one
             if (nextResource)
             {
-                parent.Map.deepResourceGrid.SetAt(cell, resDef, countPresent - 1);
-            }
-
-            var map = parent.Map;
-            for (int i = 0; i < adjCells.Count; i++)
-            {
-                // Find an output cell
-                var c = adjCells[i];
-                if (c.Walkable(map))
+                parent.Map.deepResourceGrid.SetAt(cell, resDef, count - 1);
+                // Spawn items
+                for (int i = 0; i < adjCells.Count; i++)
                 {
-                    // Try find thing of the same def
-                    var t = c.GetFirstThing(map, resDef);
-                    if (t != null)
+                    // Find an output cell
+                    var c = adjCells[i];
+                    if (c.Walkable(map))
                     {
-                        if ((t.stackCount + 1) > t.def.stackLimit)
-                            continue;
-                        // We found some, modifying stack size
-                        t.stackCount += 1;
+                        // Try find thing of the same def
+                        var t = c.GetFirstThing(map, resDef);
+                        if (t != null)
+                        {
+                            if ((t.stackCount + 1) > t.def.stackLimit)
+                                continue;
+                            // We found some, modifying stack size
+                            t.stackCount += 1;
+                        }
+                        else
+                        {
+                            // We didn't find any, creating thing
+                            t = ThingMaker.MakeThing(resDef);
+                            t.stackCount = 1;
+                            GenPlace.TryPlaceThing(t, c, map, ThingPlaceMode.Near);
+                        }
+                        break;
                     }
-                    else
-                    {
-                        // We didn't find any, creating thing
-                        t = ThingMaker.MakeThing(resDef);
-                        t.stackCount = 1;
-                        GenPlace.TryPlaceThing(t, c, map, ThingPlaceMode.Near);
-                    }
-                    break;
                 }
+                if (cycleOver) cycleOver = false;
             }
         }
 
@@ -202,10 +180,6 @@ namespace VCHE
         {
             if (parent.Spawned)
             {
-                if (noStorage)
-                {
-                    return "VCHE_CantPumpBis".Translate();
-                }
                 if (noCapacity)
                 {
                     return "VCHE_CantPump".Translate();
