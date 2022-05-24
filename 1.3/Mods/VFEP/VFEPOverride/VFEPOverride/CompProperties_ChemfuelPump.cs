cb0308b803bc52e233ac04d5896f1e35f923991c
KylianB <kylian.balde@gmail.com>
2022-05-24 14:00:04 +0200

    Type error fix

--- 1.3/Mods/VFEP/VFEPOverride/VFEPOverride/CompProperties_ChemfuelPump.cs
+++ 1.3/Mods/VFEP/VFEPOverride/VFEPOverride/CompProperties_ChemfuelPump.cs
@@ -1,7 +1,10 @@
 ﻿using PipeSystem;
 using RimWorld;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using VanillaPowerExpanded;
 using Verse;
 
